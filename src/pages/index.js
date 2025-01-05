f55c902c4a9be1d10c844b6e216b381c3557dc52
aylin.gorgun <aylingrgn2@gmail.com>
2025-01-05 14:23:18 +0300

    seo component added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ import Deployments from "../components/deployments"
 const IndexPage = () => (
   <Layout>
     <Seo
-        title="Regulus"
-        description="Advanced monitoring solutions work seamlessly in the
+      title="Regulus"
+      description="Advanced monitoring solutions work seamlessly in the
         background, detecting and alerting suspicious activities so you can
         focus on driving innovation and growth."
-      />
+    />
     <main className="flex-grow">
       <Hero />
       <Features />
