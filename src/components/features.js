a3ef06ef71b7f17e544dc896fa5d2f4fbf6024d3
aylin.gorgun <aylingrgn2@gmail.com>
2025-02-12 01:19:58 +0300

    images added to features.

--- src/components/features.js
+++ src/components/features.js
@@ -1,24 +1,65 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Features = () => {
   const featureList = [
     {
       title: "AI-Powered Transaction Monitoring",
+      image: (
+        <StaticImage
+          src="../images/transaction-monitoring.svg"
+          loading="eager"
+          width={500}
+          quality={100}
+          formats={["auto", "webp", "avif"]}
+          alt="AI-Powered Transaction Monitoring"
+        />
+      ),
       description:
-        "Regulus evaluates transactions based on predefined and custom rules, flagging high-risk entities or countries and generating real-time alerts for suspicious activities.",
+        "Regulus analyzes transactions in real-time, flagging high-risk entities and triggering alerts based on custom rules.",
     },
     {
       title: "Flexible Deployments",
+      image: (
+        <StaticImage
+          src="../images/flexible-deployments.svg"
+          loading="eager"
+          width={500}
+          quality={100}
+          formats={["auto", "webp", "avif"]}
+          alt="Flexible Deployments"
+        />
+      ),
       description:
         "Deploy Regulus in cloud, hybrid, or on-premise setups, tailored to your operational needs.",
     },
     {
       title: "Enterprise Ready",
+      image: (
+        <StaticImage
+          src="../images/enterprise-ready.svg"
+          loading="eager"
+          width={500}
+          quality={100}
+          formats={["auto", "webp", "avif"]}
+          alt="Flexible Deployments"
+        />
+      ),
       description:
         "Built for scalability and reliability, Regulus supports large-scale platforms seamlessly.",
     },
     {
       title: "Seamless Integration",
+      image: (
+        <StaticImage
+          src="../images/seamless-integration.svg"
+          loading="eager"
+          width={500}
+          quality={100}
+          formats={["auto", "webp", "avif"]}
+          alt="Flexible Deployments"
+        />
+      ),
       description:
         "Easily integrate Regulus with tools like Email, Slack, and more for a cohesive workflow.",
     },
@@ -27,15 +68,14 @@ const Features = () => {
   return (
     <section id="features" className="bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-6">
-        <h2 className="text-4xl font-extrabold mb-10 text-black">
-          Features
-        </h2>
+        <h2 className="text-4xl font-extrabold mb-10 text-black">Features</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {featureList.map((feature, idx) => (
+          {featureList?.map((feature, idx) => (
             <FeatureCard
               key={idx}
-              title={feature.title}
-              description={feature.description}
+              image={feature?.image}
+              title={feature?.title}
+              description={feature?.description}
             />
           ))}
         </div>
@@ -44,10 +84,13 @@ const Features = () => {
   )
 }
 
-const FeatureCard = ({ title, description }) => (
-  <div className="bg-white p-6 rounded-lg shadow hover:shadow-xl transition duration-300">
-    <h3 className="text-xl font-bold mb-2 text-gray-800">{title}</h3>
-    <p className="text-gray-600">{description}</p>
+const FeatureCard = ({ image, title, description }) => (
+  <div className="bg-white rounded-lg shadow hover:shadow-xl transition duration-300 w-full max-w-3xl mx-auto">
+    <div className="w-full flex justify-center items-center mb-4">{image}</div>
+    <div className="p-6">
+      <h3 className="text-2xl font-bold mb-2 text-gray-800">{title}</h3>
+      <p className="text-gray-600 text-lg">{description}</p>
+    </div>
   </div>
 )
 
